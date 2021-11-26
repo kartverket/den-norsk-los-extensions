938cab533b11666e82e898d28868da059e7441f7
Håvard Vidme <havard.vidme@kartverket.no>
2021-11-26 11:39:20 +0100

    Change placename API

--- directus-ssr-search/src/input.vue
+++ directus-ssr-search/src/input.vue
@@ -26,7 +26,6 @@
 
 <script>
 import mixin from "@directus/extension-toolkit/mixins/interface";
-import { parseString } from "xml2js";
 export default {
   mixins: [mixin],
   data() {
@@ -174,84 +173,51 @@ export default {
       // console.log("getPlacenames:");
 
       try {
-        let response = "";
-
+        const search = urlSearchParams();
+        search.set('utkoordsys', 4258);
         if (isNum) {
-          response = await fetch(
-            "https://ws.geonorge.no/SKWS3Index/v2/ssr/sok?stedsnummer=" +
-              query +
-              "&epsgKode=4258&eksakteForst=true",
-            {
-              cache: "default",
-              dataType: "xml"
-            }
-          );
+          search.set('stedsnummer', query);
         } else {
-          response = await fetch(
-            "https://ws.geonorge.no/SKWS3Index/v2/ssr/sok?navn=" +
-              query +
-              "*&epsgKode=4258&eksakteForst=true",
-            {
-              cache: "default",
-              dataType: "xml"
-            }
-          );
+          search.set('sok', query);
+          search.set('fuzzy', true);
         }
 
-        if (response !== "") {
-          const data = await response.text();
-          parseString(data, function(err, result) {
-            if (result) {
-              // console.log(result,err);
-              if (result.sokRes.stedsnavn) {
-                if (result.sokRes.stedsnavn.length < resultLimit) {
-                  resultLimit = result.sokRes.stedsnavn.length;
-                }
+        const url = new URL('/stedsnavn/v1/sted', 'https://ws.geonorge.no');
+        url.search = search.toString();
 
-                for (let r = 0; r < resultLimit; r++) {
-                  let placeNameResults = {};
+        const response = await fetch(url, {
+          cache: 'default',
+        });
+        const data = await response.json();
 
-                  const placeName = result.sokRes.stedsnavn[r].stedsnavn[0];
-                  const placeKommune =
-                    result.sokRes.stedsnavn[r].kommunenavn[0];
-                  const placeFylke = result.sokRes.stedsnavn[r].fylkesnavn[0];
-                  let placeLat = result.sokRes.stedsnavn[r].nord[0];
-                  let placeLng = result.sokRes.stedsnavn[r].aust[0];
-                  const nameType = result.sokRes.stedsnavn[r].navnetype[0];
-                  const placeNumber = result.sokRes.stedsnavn[r].stedsnummer[0];
-                  const nameStatus =
-                    result.sokRes.stedsnavn[r].skrivemaatestatus[0];
+        if (data.metadata.totaltAntallTreff > 0) {
+          const total = data.navn.length;
+          for (let i = 0; i < total; i++) {
+            const placeNameResults = {};
+            placeNameResults.id = i;
 
-                  placeNameResults.id = r;
+            const placeName = data['sted'][i]['stedsnavn'][0]['skrivemåte'];
+            const placeKommune = data['kommuner'][0]['kommunenavn'];
+            const placeFylke = data['fylker'][0]['fylkesnavn'];
+            const placeLat = parseFloat(data['navn'][i]['representasjonspunkt']['nord']).toFixed(4);
+            const placeLng = parseFloat(data['navn'][i]['representasjonspunkt']['øst']).toFixed(4);
+            const nameType = data['navn'][i]['navneobjekttype'];
+            const placeNumber = data['navn'][i]['stedsnummer'];
+            const nameStatus = data['navn'][i]['stedsnavn'][0]['skrivemåtestatus'];
 
-                  placeLat = parseFloat(placeLat).toFixed(4);
-                  placeLng = parseFloat(placeLng).toFixed(4);
+            placeNameResults.label = `${placeName}, ${placeKommune}, ${placeFylke} - ${nameType} - `;
+            placeNameResults.value = `${placeLat}, ${placeLng} - ${placeNumber}`;
 
-                  placeNameResults.label =
-                    placeName +
-                    ", " +
-                    placeKommune +
-                    ", " +
-                    placeFylke +
-                    " - " +
-                    nameType +
-                    " - ";
-                  placeNameResults.value =
-                    placeLat + ", " + placeLng + " - " + placeNumber;
+            const checkForUnwanted = unwantedPlace.indexOf(nameType);
+            const checkForConfirmed = confirmedPlace.indexOf(nameStatus);
 
-                  const checkForUnwanted = unwantedPlace.indexOf(nameType);
-                  const checkForConfirmed = confirmedPlace.indexOf(nameStatus);
-
-                  if (checkForUnwanted === -1 && checkForConfirmed !== -1) {
-                    ssrResults.push(placeNameResults);
-                  }
-                }
-              }
+            if (checkForUnwanted === -1 && checkForConfirmed !== -1) {
+              ssrResults.push(placeNameResults);
             }
-          });
-          this.placenames = ssrResults;
+          }
         }
-        // console.log("data: ", ssrResults);
+
+        this.placenames = ssrResults;
       } catch (error) {
         console.error(error);
       }
